d8a278da573478a72bc8f3c91471e3a38c05805e
mandress1 <matthew-andress@uiowa.edu>
2016-09-26 19:04:46 -0500

    main project file. most everything will be in here

---
+++ commNet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SCHED_PORT 23510 // ScheduleServer port. host: c-lnx001.engr.uiowa.edu
+#define WEATH_PORT 23511 // WeatherServer port. host: c-lnx000.engr.uiowa.edu
+
+int main(int argc, char* argv[])
+{
+    char* hawkid;
+    
+    return 0;
+}
