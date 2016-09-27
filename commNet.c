a779885b12b829ecc41bff31c5c4d1c7b55bd8ba
mandress1 <matthew-andress@uiowa.edu>
2016-09-26 21:17:20 -0500

    stuck trying to open udp socket

--- commNet.c
+++ commNet.c
@@ -7,11 +7,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define SCHED_PORT 23510 // ScheduleServer port. host: c-lnx001.engr.uiowa.edu
-#define WEATH_PORT 23511 // WeatherServer port. host: c-lnx000.engr.uiowa.edu
+#define SCHED_PORT 23510    // ScheduleServer port. host: c-lnx001.engr.uiowa.edu
+#define WEATH_PORT 23511    // WeatherServer port. host: c-lnx000.engr.uiowa.edu
+#define SIZE 1024
+char buff[SIZE];            // Because why not have global variables
 
 int main(int argc, char* argv[])
 {
+    int sockfd, client_sockfd;
+    int nread, len;         // Have these because example on slides
+    time_t time;            // Same with this one too
+    struct sockaddr_in serv_addr, client_addr; // These too
     char* hawkid;
 
     if (argc == 2)
@@ -24,6 +30,14 @@ int main(int argc, char* argv[])
         printf("Error: Incorrect number of arguments supplied\n");
         return -1;
     }
+
+    printf("Trying to open ScheduleServer\n");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("Unable to open socket for ScheduleServer\n");
+        return 2;
+    }
+    printf("Socket opened\n");
 
     return 0;
 }
