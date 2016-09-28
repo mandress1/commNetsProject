278f04bf9f726f231e42b54aa099386c3c99ba22
mandress1 <matthew-andress@uiowa.edu>
2016-09-27 20:16:06 -0500

    alright alright alright, tcptest is now working. on to udp. again...

--- tcptest.c
+++ tcptest.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #define SCHED_PORT 23510    // ScheduleServer port. host: c-lnx001.engr.uiowa.edu
 #define WEATH_PORT 23511    // WeatherServer port. host: c-lnx000.engr.uiowa.edu
@@ -17,6 +17,41 @@ const char* WEATH_NAME = "c-lnx000.engr.uiowa.edu";
 
 int main(void)
 {
+    int sockfd;
+    int nread;
+    struct sockaddr_in servAddr;
+    struct hostent* h;
+    char* tstmsg = "mandress";
 
-	return 0;
+    printf("Trying to get socket\n");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Error: couldn't open socket\n");
+        exit(1);
+    }
+    printf("Socket opened!\n");
+
+    printf("Setting up server shit\n");
+    h = gethostbyname(WEATH_NAME);
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(WEATH_PORT);
+    bcopy(h->h_addr, (char*)&servAddr.sin_addr, h->h_length);
+
+    printf("Attempting to connect...\n");
+    if(connect(sockfd, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+    {
+        printf("Error: enable to establish connection\n");
+        exit(2);
+    }
+    printf("Connection established\n");
+
+    printf("Attemping to send data: %s\n", tstmsg);
+    write(sockfd, tstmsg, sizeof(tstmsg));
+    printf("Message sent\nAttempting to get response\n");
+    nread = read(sockfd, buff, SIZE);
+    buff[nread] = '\0';
+    printf("Response: %s\n", buff);
+
+    close(sockfd);
+    return 0;
 }
