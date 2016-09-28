acfeb53b6b651e1ce81b58746763d351773621aa
mandress1 <matthew-andress@uiowa.edu>
2016-09-27 22:10:35 -0500

    Not much added. start again tomorrow

--- commNetProj.c
+++ commNetProj.c
@@ -22,8 +22,15 @@ int main(int argc, char* argv[])
     struct hostent* h;              // Used for getting servers
     struct sockaddr_in schedAddr;   // Socket for ScheduleServer
     struct sockaddr_in weathAddr;   // Socket for WeatherServer
+    char* hawkid;
     char* msgToSend;
 
-    
+    if (argc < 2)
+    {
+        printf("Error: not enough arguments supplied.\nUsage: `./<executable_name>.exe <hawkid>\n");
+        exit(-1);
+    }
+
+
     return 0;
 }
