948150c43e4bda3e59e6f38569486779d65c1568
mandress1 <matthew-andress@uiowa.edu>
2016-09-29 23:19:28 -0500

    finally done. woot woot

--- commNetProj.c
+++ commNetProj.c
@@ -1,3 +1,16 @@
+/**********************************************************************************************************
+ *  How to compile:                                                                                       *
+ *          While in the same directory as the c file, type this command: gcc commNetProj.c -o client.exe *
+ *                                                                                                        *
+ *                                                                                                        *
+ *  How to run:                                                                                           *
+ *          While in the same directory as the executable, this command: ./client.exe <hawkid>            *
+ *          example: ./client.exe mandress                                                                *
+ *                                                                                                        *
+ *  Programmer:                                                                                           *
+ *          Name: Matt Andress                                                                            *
+ *          ID:   00797286                                                                                *  
+ **********************************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,15 +20,16 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define CONF_SIZE       14
-#define SCHED_PORT      23510    // ScheduleServer port. host: c-lnx001.engr.uiowa.edu
-#define WEATH_PORT      23511    // WeatherServer port. host: c-lnx000.engr.uiowa.edu
-#define IN_BUFF_SIZE    16384     // buffer size
-#define OUT_BUFF_SIZE   256
+
+#define CONF_SIZE       14      // Size of big 10 conference
+#define SCHED_PORT      23510   // ScheduleServer port
+#define WEATH_PORT      23511   // WeatherServer port
+#define IN_BUFF_SIZE    16384   // Buffer size for reading responses from server
+#define OUT_BUFF_SIZE   256     // Buffer size for sending output to server
 
 
-const char* SCHED_NAME = "c-lnx001.engr.uiowa.edu";
-const char* WEATH_NAME = "c-lnx000.engr.uiowa.edu";
+const char* SCHED_NAME = "c-lnx001.engr.uiowa.edu";     // ScheduleServer host name
+const char* WEATH_NAME = "c-lnx000.engr.uiowa.edu";     // WeatherServer host name
 const char* SCHOOLS[] = {"Michigan","Indiana","Illinois","Maryland","Ohio_State","Northwestern","Iowa","Purdue","Michigan_State","Nebraska","Wisconsin","Minnesota","Rutgers","Penn_State"};
 
 
@@ -26,17 +40,16 @@ char* remNull(char* str);
 
 int main(int argc, char* argv[])
 {
-    int i;
-    int schedfd;                    // ScheduleServer socket file descriptor
-    int weathfd;                    // WeatherServer socket file descriptor
-    int nread;
-    struct sockaddr_in schedAddr;   // Socket for ScheduleServer
-    struct sockaddr_in weathAddr;   // Socket for WeatherServer
-    socklen_t schedLen;
+    int schedfd;                                                // ScheduleServer socket file descriptor
+    int weathfd;                                                // WeatherServer socket file descriptor
+    int nread;                                                  // Number of bytes read from servers
+    struct sockaddr_in schedAddr;                               // Socket for ScheduleServer
+    struct sockaddr_in weathAddr;                               // Socket for WeatherServer
+    socklen_t schedLen;                                         // Size of ScheduleServer socket struct
     char* hawkid;
-    char* respBuff = (char*)malloc(IN_BUFF_SIZE*sizeof(char));
-    char* outBuff = (char*)malloc(OUT_BUFF_SIZE*sizeof(char));
-    char* usrInput = (char*)malloc(OUT_BUFF_SIZE*sizeof(char));
+    char* respBuff = (char*)malloc(IN_BUFF_SIZE*sizeof(char));  // Buffer for getting responses from server
+    char* outBuff = (char*)malloc(OUT_BUFF_SIZE*sizeof(char));  // Buffer for outputing to servers
+    char* usrInput = (char*)malloc(OUT_BUFF_SIZE*sizeof(char)); // Buffer for getting input from user
 
     if(argc < 2)
     {
@@ -44,19 +57,15 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    for(i = 0;i < strlen(argv[1]);i++)
-    {
-        usrInput[i] = argv[1][i];
-    }
     hawkid = argv[1];
 
-    /* setting udp first ScheduleServer */
+
     printf("Setting up socket for UDP connection\n");
     schedfd = setUpConnection(&schedAddr, SOCK_DGRAM, SCHED_NAME, SCHED_PORT);
     schedLen = sizeof(schedAddr);
     printf("ScheduleServer socket set up\n");
 
-    /* setting up WeatherServer */
+    
     printf("Setting up socket for TCP connection\n");
     weathfd = setUpConnection(&weathAddr, SOCK_STREAM, WEATH_NAME, WEATH_PORT);
     printf("Connection made to WeatherServer\n");
@@ -68,24 +77,26 @@ int main(int argc, char* argv[])
     printf("WeatherServer says: '%s'\n", respBuff);
     
 
-    while(strncmp(usrInput, "quit", min(strlen(usrInput), strlen("quit"))))
+    // Main loop for program.
+    do
     {
-        bzero(usrInput, OUT_BUFF_SIZE);
+        /* Reseting buffers for next input */
+        bzero(usrInput, OUT_BUFF_SIZE);                         
         bzero(respBuff, IN_BUFF_SIZE);
         bzero(outBuff, OUT_BUFF_SIZE);
 
         printf("Enter a School name for weather forcast or \"quit\" to exit\nEntry: ");
         scanf("%s", usrInput);
-        printf("after\n");
 
-        if(isValidSchoolName(usrInput, SCHOOLS, CONF_SIZE))
+        if(isValidSchoolName(usrInput, SCHOOLS, CONF_SIZE))                 // User enters valid school
         {
-            // send <hawkid> <schoolName> to ScheduleServer
+            // send '<hawkid> <schoolName>' to ScheduleServer
             int i, j;
             int idLen = strlen(hawkid);
             int inLen = strlen(usrInput);
             int totLen = idLen + inLen + 1;
 
+            // Peraring output
             for(i = 0;i < idLen;i++)
             {
                 outBuff[i] = hawkid[i];
@@ -95,6 +106,8 @@ int main(int argc, char* argv[])
             {
                 outBuff[idLen + i + 1] = usrInput[i];
             }
+
+            // Sending output to SceduleServer
             sendto(schedfd, outBuff, totLen, 0, (struct sockaddr*)&schedAddr, sizeof(schedAddr));
             outBuff[totLen] = '\0';
             printf("'%s' sent to ScheduleServer\n", outBuff);
@@ -103,47 +116,73 @@ int main(int argc, char* argv[])
             nread = recvfrom(schedfd, respBuff, IN_BUFF_SIZE, 0, (struct sockaddr*)&schedAddr, &schedLen);
             respBuff[nread] = '\0';
             printf("ScheduleServer says: '%s\n", respBuff);
+
+            // Preping output to WeatherServer
             for(i = strlen(respBuff)- 3,j = 0;i < strlen(respBuff);i++, j++)
             {
                 outBuff[j] = respBuff[i];
             }
             
-            // send <3 letter code> to WeatherServer
+            // send 3 letter code to WeatherServer
             write(weathfd, outBuff, j);
             outBuff[j] = '\0';
             printf("'%s' sent to WeatherServer\n", outBuff);
-            sleep(1);
+            sleep(1);       // Sleep cuz why not 
+
             // print weather server response
             nread = read(weathfd, respBuff, IN_BUFF_SIZE);
             respBuff[nread] = '\0';
             printf("WeatherServer says: %s\n", respBuff);
-            printf(" \n");
+            //printf(" \n");
         }
-        else if(strncmp(usrInput, "quit", min(strlen(usrInput), strlen("quit"))) == 0)
+        else if(strncmp(usrInput, "quit", min(strlen(usrInput), strlen("quit"))) == 0)      // User decides to quit
         {
             // send quit to WeatherServer
             write(weathfd, remNull(usrInput), sizeof(remNull(usrInput)));
             printf("'%s' sent to WeatherServer\n", usrInput);
+
             // tell user goodby
             nread = read(weathfd, respBuff, IN_BUFF_SIZE);
             respBuff[nread] = '\0';
 
             printf("WeatherServer says: '%s'\nNow exiting the program\n", respBuff);
         }
-        else
+        else        // User is doesn't enter a valid choice
         {
-            printf("Error: input nor recognized.\nAll spaces must be underscores ('_') and school names must be correctly capitalized\n");
+            printf("Error: input nor recognized.\nAll spaces must be underscores ('_') and school names must be properlly capitalized\n");
         }
         
-    }
+    }while(strncmp(usrInput, "quit", min(strlen(usrInput), strlen("quit"))));
     
-
+    // Freeing allocated memory
+    free(usrInput);
+    free(respBuff);
+    free(outBuff);
 
     close(weathfd);
     close(schedfd);
     return 0;
 }
 
+/*********************************************************************
+ *  Description:                                                     *
+ *      This function handles setting up sockets for both            *
+ *      TCP and UDP.                                                 *
+ *                                                                   *
+ *  Arguments:                                                       *
+ *      struct sockaddr_in* toSetUp:                                 *
+ *                  pointer to socket to be set up                   *
+ *      int mode:                                                    *
+ *                  Mode for socket to be sut up as DGRAM or STREAM  *
+ *      const char* hostName:                                        *
+ *                  Name of hose to connect socket to                *
+ *      int hostPort:                                                *
+ *                  Port that the host is listening on               *
+ *                                                                   *
+ *  Return:                                                          *
+ *      int fd:                                                      *
+ *                  file descriptor returned from setting up socket  *
+ *********************************************************************/
 int setUpConnection(struct sockaddr_in* toSetUp, int mode, const char* hostName, int hostPort)
 {
     int fd = 0;
@@ -185,6 +224,23 @@ int setUpConnection(struct sockaddr_in* toSetUp, int mode, const char* hostName,
     return fd;
 }
 
+/*********************************************************************
+ *  Desctiption:                                                     *
+ *      This function checks to see if the input given by the user   *
+ *      checks to see if it as a valid Big 10 school.                *
+ *                                                                   *
+ *  Arguments:                                                       *
+ *      const char* schoolName:                                      *
+ *              User's input to be validated                         *
+ *      const char* validSchools[]:                                  *
+ *              An array of the valid Big 10 school names            *
+ *      int confSize:                                                *
+ *              Number of teams in the conference                    *
+ *                                                                   *
+ *  Returns:                                                         *
+ *      1 if the user entered a valid school name                    *
+ *      0 otherwise                                                  *
+ *********************************************************************/
 int isValidSchoolName(const char* schoolName, const char* validSchools[], int confSize)
 {
     int i, nToCmp;
@@ -199,14 +255,39 @@ int isValidSchoolName(const char* schoolName, const char* validSchools[], int co
     return 0;
 }
 
+/*********************************************************************
+ *  Desctription:                                                    *
+ *      This function compares the two integers and returns the      *
+ *      lowest value of the two                                      *
+ *                                                                   *
+ *  Arguments:                                                       *
+ *      int one:                                                     *
+ *          The first value to be compared                           *
+ *      int two:                                                     *
+ *                                                                   *
+ *  Return:                                                          *
+ *      int: THe smaller of the two values                           *
+ *********************************************************************/
 int min(int one, int two)
 {
     return (one < two)?(one):(two);
 }
 
+/*********************************************************************
+ * Description:                                                      *
+ *      This function takes in a string and returns a string without *
+ *      the null characer at the end                                 *
+ *                                                                   *
+ *  Arguments:                                                       *
+ *      char* str:                                                   *
+ *              String to bo copied without the null character       *
+ *                                                                   *
+ *  Return:                                                          *
+ *      String without the null character at the end                 *
+ *********************************************************************/
 char* remNull(char* str)
 {
-    int ogLen = strlen(str), i;
+    int ogLen = sizeof(str)/sizeof(str[0]), i;
     char* newStr = (char*)malloc(ogLen*sizeof(char));
 
     for(i = 0;i < ogLen;i++)
